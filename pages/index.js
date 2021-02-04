2b9cc9c05feff4872dbe93caaff9146b46fc1a68
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-05 00:04:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 /** @format */
-import { useState, useEffect } from 'react';
+
+// /** @format */
+// import { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import Prices from '../components/Prices';
-const Index = () => {
-	const [data, setData] = useState();
-	const fetchData = async () => {
-		const res = await fetch(
-			'https://api.coindesk.com/v1/bpi/currentprice.json'
-		);
-		const resData = await res.json();
-		setData(resData);
-	};
-	useEffect(() => {
-		fetchData();
-		console.log('want data call', data);
-	}, []);
+const Index = ({ data }) => {
+	// const [data, setData] = useState();
+	// const fetchData = async () => {
+	// 	const res = await fetch(
+	// 		'https://api.coindesk.com/v1/bpi/currentprice.json'
+	// 	);
+	// 	const resData = await res.json();
+	// 	setData(resData);
+	// };
+	// useEffect(() => {
+	// 	fetchData();
+	// 	console.log('want data call', data);
+	// }, []);
 	return (
 		<Layout>
 			<h1>Welcome to {data?.chartName}</h1>
@@ -22,5 +24,15 @@ const Index = () => {
 			<h2>updated at :{data?.time?.updated}</h2>
 		</Layout>
 	);
+};
+export const getStaticProps = async () => {
+	const res = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`);
+	const data = await res.json();
+
+	return {
+		props: {
+			data,
+		},
+	};
 };
 export default Index;
