d5fe6d2021df4c68e830a98fd4da8b3ab3985a83
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-22 08:59:37 +0530

    Create bitcoin.js

---
+++ pages/bitcoin.js
@@ -0,0 +1,38 @@
+/** @format */
+
+// /** @format */
+// import { useState, useEffect } from 'react';
+import Layout from '../components/Layout';
+import Prices from '../components/Prices';
+const Index = ({ data }) => {
+	// const [data, setData] = useState();
+	// const fetchData = async () => {
+	// 	const res = await fetch(
+	// 		'https://api.coindesk.com/v1/bpi/currentprice.json'
+	// 	);
+	// 	const resData = await res.json();
+	// 	setData(resData);
+	// };
+	// useEffect(() => {
+	// 	fetchData();
+	// 	console.log('want data call', data);
+	// }, []);
+	return (
+		<Layout>
+			<h1>Welcome to {data?.chartName}</h1>
+			<Prices bpi={data?.bpi} />
+			<h2>updated at :{data?.time?.updated}</h2>
+		</Layout>
+	);
+};
+export const getStaticProps = async () => {
+	const res = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`);
+	const data = await res.json();
+
+	return {
+		props: {
+			data,
+		},
+	};
+};
+export default Index;
